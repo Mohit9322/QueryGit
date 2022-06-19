e8743ff98166674a02b07fc771f6ecec0747f176
Mohit <mohittyagics@gmail.com>
2022-06-19 11:13:43 +0530

    changes committed

--- GitQuery/GitQuery/ViewController2.swift
+++ GitQuery/GitQuery/ViewController2.swift
@@ -13,7 +13,7 @@ class ViewController2: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print("Commit changes")
         // Do any additional setup after loading the view.
     }
     
